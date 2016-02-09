99fc111f42ee63ac729df393d764d4e0754b2811
Karol Altamirano <karol.altamirano@unit9.com>
2016-02-09 18:08:54 +0100

    Fix webpack public path

--- app/templates/common/gulpfile.js
+++ app/templates/common/gulpfile.js
@@ -80,7 +80,7 @@ var TASK_NOTIFICATION = false,
 
 myConfig.output = {
     path: BUILD_DIR + '/scripts',
-    publicPath: 'scripts/',
+    publicPath: '/scripts/',
     filename: '[name].js',
     sourceMapFilename: 'maps/[file].map'
 };
