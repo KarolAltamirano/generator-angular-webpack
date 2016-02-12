8973a5d44688c17d227cf2f43958a032cd33b96a
Karol Altamirano <karol.altamirano@unit9.com>
2016-02-12 22:59:52 +0100

    Version info in console

--- app/templates/common/src/scripts/utilities/version.js
+++ app/templates/common/src/scripts/utilities/version.js
@@ -9,6 +9,15 @@ var version = function () {
         return;
     }
 
+    if (console && console.log) {
+        console.log(
+            '%cv' + versionJson.version + '%c | %c' + versionJson.time,
+            'color: #ffffff; background: #00aa00; padding: 0 5px;',
+            'color: #666666; background: #ffffff;',
+            'color: #ffffff; background: #00aa00; padding: 0 5px;'
+        );
+    }
+
     $('body').append(
         '<div class="version">' +
             'v' + versionJson.version + ' <span>| ' + versionJson.time + '</span>' +
